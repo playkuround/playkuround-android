2ad822974823f0321ef055c1c6e1959b2998f702
SangMin Kang <rdrniel12345@gmail.com>
2022-08-22 22:39:42 +0900

    feat : add user register api response data class

--- app/src/main/java/com/umc/playkuround/service/RegisterUserResponse.kt
+++ app/src/main/java/com/umc/playkuround/service/RegisterUserResponse.kt
@@ -1,2 +1,23 @@
 package com.umc.playkuround.service
 
+import com.google.gson.annotations.SerializedName
+
+data class RegisterUserResponse(
+    @SerializedName(value = "isSuccess") val isSuccess : Boolean,
+    @SerializedName(value = "response") val response : Response,
+    @SerializedName(value = "errorResponse") val errorResponse : ErrorResponse
+)
+
+data class Response(
+    @SerializedName(value = "grantType") val grantType : String,
+    @SerializedName(value = "accessToken") val accessToken : String,
+    @SerializedName(value = "accessTokenExpiredAt") val accessTokenExpiredAt : String,
+    @SerializedName(value = "refreshToken") val refreshToken : String,
+    @SerializedName(value = "refreshTokenExpiredAt") val refreshTokenExpiredAt : String
+)
+
+data class ErrorResponse(
+    @SerializedName(value = "status") val status : Int,
+    @SerializedName(value = "code") val code : String,
+    @SerializedName(value = "message") val message : String
+)
