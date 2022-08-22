31297b50d7d38a8a2926f68f9c0ba370f5178551
Sang-Min Kang <81908212+isoo127@users.noreply.github.com>
2022-08-22 22:07:32 +0900

    Feat/login (#4)
    * feat : construct login activity layout
    feat : complete function of login activity
    
    * feat : add color/button_green.xml that is color selector resource
    fix : fix drawable/button_green.xml which is selector enabled not working
    
    * docs : edit_text add drawable & fix color/button_green
    
    * docs : add edit text wrong value drawable file
    
    * feat : construct email certify activity
    
    * docs : add permission to link internet
    
    * feat : complete functions of email certify activity

--- app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
@@ -16,8 +16,9 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = Intent(this, MiniGameQuizActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+        finish()
     }
 
 }
