369caedd0554b3f07b81980901c1a6017038f62b
sieux2 <zosel154@naver.com>
2022-08-28 18:17:29 +0900

    feat: rank info xml and spinner dimen update

--- app/src/main/java/com/umc/playkuround/activity/RankingInfoActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/RankingInfoActivity.kt
@@ -1,5 +1,6 @@
 package com.umc.playkuround.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.umc.playkuround.databinding.ActivityRankingInfoBinding
@@ -12,6 +13,11 @@ class RankingInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRankingInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rankingXBt.setOnClickListener {
+            val intent = Intent(this, PolicyAgreeActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
