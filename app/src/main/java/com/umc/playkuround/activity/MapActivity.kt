2c58825a088e7a5bf3482e286b5675bc1f1bab04
Sang-Min Kang <rdrniel12345@gmail.com>
2022-08-30 20:32:23 +0900

    feat : link mini games to map

--- app/src/main/java/com/umc/playkuround/activity/MapActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/MapActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.umc.playkuround.databinding.ActivityMapBinding
+import kotlin.random.Random
 
 class MapActivity : AppCompatActivity() {
 
@@ -18,7 +19,28 @@ class MapActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        binding.mapClickBtn.setOnClickListener {
+            val intent : Intent = when (getGames()) {
+                0 -> {
+                    Intent(this, MiniGameQuizActivity::class.java)
+                }
+                1 -> {
+                    Intent(this, MiniGameMoonActivity::class.java)
+                }
+                2 -> {
+                    Intent(this, MiniGameTimerActivity::class.java)
+                }
+                else -> {
+                    Intent(this, MiniGameTimerActivity::class.java)
+                }
+            }
+            startActivity(intent)
+        }
     }
 
+    private fun getGames() : Int {
+        return Random(System.currentTimeMillis()).nextInt(3)
+    }
 
 }
