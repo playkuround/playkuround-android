4e4991ddaafbe10c8e265e1084c19230daf78d6a
Sang-Min Kang <rdrniel12345@gmail.com>
2022-09-05 20:54:20 +0900

    fix : changed green button pressed color

--- app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
@@ -18,7 +18,7 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = Intent(this,MainActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
     }
