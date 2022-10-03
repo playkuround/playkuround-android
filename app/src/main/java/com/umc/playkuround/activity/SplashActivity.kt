9277ec99dd10d98b61ca3caf554fb4e2f6e0a575
sieux2 <zosel154@naver.com>
2022-10-03 21:35:16 +0900

    docs:major choice dropdown change

--- app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
@@ -18,7 +18,7 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, MajorChoiceActivity::class.java)
         startActivity(intent)
         finish()
     }
