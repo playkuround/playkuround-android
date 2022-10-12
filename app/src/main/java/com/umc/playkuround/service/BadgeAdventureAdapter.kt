16c319ee124518c3b7ca9d43ab4fabf9811049ff
sieux2 <zosel154@naver.com>
2022-10-12 15:54:45 +0900

    feat: badgefragment adverture badge update and adapter Link

---
+++ app/src/main/java/com/umc/playkuround/service/BadgeAdventureAdapter.kt
@@ -0,0 +1,31 @@
+package com.umc.playkuround.service
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.umc.playkuround.R
+import com.umc.playkuround.databinding.ListItemBadgeAttendanceBinding
+
+class AdListAdapterGrid(): RecyclerView.Adapter<AdListAdapterGrid.ViewHolder>() {
+
+    inner class ViewHolder(val binding : ListItemBadgeAttendanceBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind() {
+            binding.listItemBadgeLoked.setImageResource(R.drawable.badge_locked)
+        }
+
+    }
+
+    class GridAdapter(val layout: View): RecyclerView.ViewHolder(layout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListItemBadgeAttendanceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount() = 10
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind()
+    }
+}
