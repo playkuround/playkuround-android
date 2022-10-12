199e2700071099a413f7d3ae2e95a91bd4e93672
sieux2 <zosel154@naver.com>
2022-10-12 15:34:34 +0900

    docs: badge interval update

--- app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
@@ -20,7 +20,7 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = Intent(this, MiniGameMoonActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
