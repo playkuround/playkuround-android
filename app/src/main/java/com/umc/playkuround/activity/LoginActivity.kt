058ae2c9101182acf282058c8fb90dd4a61eb685
Sang-Min Kang <rdrniel12345@gmail.com>
2022-11-14 21:45:10 +0900

    delete test code

--- app/src/main/java/com/umc/playkuround/activity/LoginActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/LoginActivity.kt
@@ -53,58 +53,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun testing() {
-        /*binding.loginLogoIv.setOnClickListener {
-            val userService = UserService()
-            val temp = User("test21@test.com", "test21", "컴퓨터공학부", null)
-            userService.setOnResponseListener(object : UserService.OnResponseListener() {
-                override fun <T> getResponseBody(body : T, isSuccess : Boolean, err : String) {
-                    if(isSuccess) {
-                        if(body is UserTokenResponse)
-                            temp.userTokenResponse = body
-                        user = temp.copy()
-                        user.save(pref)
-                        Log.d("userInfo", "onCreate: $user")
-                    } else {
-                        Log.d("retrofit", "getResponseBody: $err")
-                        Toast.makeText(applicationContext, err, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }).register(temp)
-        }*/
-
-        /*binding.loginLogoIv.setOnClickListener {
-            val userService = UserService()
-            val nickname = "test17"
-            userService.setOnResponseListener(object : UserService.OnResponseListener() {
-                override fun <T> getResponseBody(body: T, isSuccess: Boolean, err: String) {
-                    if(isSuccess) {
-                        if(body is DuplicateResponse)
-                            Log.d("retrofit", "getResponseBody: " + body.response)
-                    } else {
-                        Toast.makeText(applicationContext, err, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }).isDuplicate(nickname)
-        }*/
-
-        binding.loginLoginBtn.setOnClickListener {
-            val userService = UserService()
-            val token = user.getAccessToken()
-            userService.setOnResponseListener(object : UserService.OnResponseListener() {
-                override fun <T> getResponseBody(body: T, isSuccess: Boolean, err: String) {
-                    if(isSuccess) {
-                        if(body is UserTokenResponse)
-                            user.userTokenResponse = body
-                        user.load(pref)
-                        Log.d("userInfo", "onCreate: $user")
-                    } else {
-                        Toast.makeText(applicationContext, err, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }).login(token)
-        }
-
-        /*binding.loginLogoIv.setOnClickListener {
+        binding.loginLogoIv.setOnClickListener {
             val userService = UserService()
             val token = user.getAccessToken()
             userService.setOnResponseListener(object : UserService.OnResponseListener() {
@@ -116,7 +65,7 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
             }).logout(token)
-        }*/
+        }
     }
 
 }
