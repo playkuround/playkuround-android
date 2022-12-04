d98fc0c947ee2050f3043e154ddbd13e2256c03c
sieux2 <zosel154@naver.com>
2022-12-04 17:06:44 +0900

    docs: small dialog xml update and feat: attadence click map_place_info_dialog

--- app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/SplashActivity.kt
@@ -34,7 +34,7 @@ class SplashActivity : AppCompatActivity() {
         user.load(pref)
         Log.d("user info", "checkLoginInfo: email : ${user.email}, name : ${user.nickname}, major : ${user.major}")
         if(user.major == "null") {
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         } else {
             login()
--- app/src/main/java/com/umc/playkuround/fragment/BadgeFragment.kt
+++ app/src/main/java/com/umc/playkuround/fragment/BadgeFragment.kt
@@ -1,14 +1,22 @@
 package com.umc.playkuround.fragment
 
+import android.app.Application
+import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.umc.playkuround.R
 import com.umc.playkuround.databinding.FragmentBadgeBinding
+import com.umc.playkuround.dialog.SlideUpDialog
+import com.umc.playkuround.dialog.smallSlideUpDialog
 import com.umc.playkuround.service.AdListAdapterGrid
 import com.umc.playkuround.service.ListAdapterGrid
 
@@ -26,6 +34,12 @@ class BadgeFragment : Fragment() {
         binding.badgeAttendanceRv.adapter = ListAdapterGrid()
         val atLayoutManager = GridLayoutManager(context, 3)
         binding.badgeAttendanceRv.layoutManager = atLayoutManager
+
+        binding.badgeAttendanceTv.setOnClickListener {
+            placeinfoDialog()
+        }
+
+
 
         binding.badgeAttendanceRv.addItemDecoration(object : RecyclerView.ItemDecoration() {
             override fun getItemOffsets(
@@ -60,6 +74,24 @@ class BadgeFragment : Fragment() {
         })
 
         return binding.root
+
+
     }
 
+    private fun placeinfoDialog() {
+        var contentView: View =
+            (activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
+                R.layout.dialog_map_place, null
+            )
+        val slideupPopup = smallSlideUpDialog.Builder(requireContext())
+            .setContentView(contentView)
+            .create()
+        slideupPopup.setCancelable(false)
+
+        slideupPopup.show()
+    }
+
+
+
 }
+
