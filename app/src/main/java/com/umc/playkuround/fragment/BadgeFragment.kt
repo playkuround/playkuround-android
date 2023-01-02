f9cad6b6c4d09a71c40eeca7f4f5576641a87879
sieux2 <zosel154@naver.com>
2023-01-02 19:04:33 +0900

    test: badge empty text

--- app/src/main/java/com/umc/playkuround/fragment/BadgeFragment.kt
+++ app/src/main/java/com/umc/playkuround/fragment/BadgeFragment.kt
@@ -131,7 +131,7 @@ class BadgeFragment : Fragment() {
             startActivity(intent)
 
         }
-        
+
 
 
 
--- app/src/main/java/com/umc/playkuround/fragment/RankingFragment.kt
+++ app/src/main/java/com/umc/playkuround/fragment/RankingFragment.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isEmpty
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.umc.playkuround.activity.RankingInfoActivity
+import com.umc.playkuround.data.User
 import com.umc.playkuround.databinding.FragmentRankingBinding
 import com.umc.playkuround.service.RankingRVAdapter
 
@@ -21,12 +24,32 @@ class RankingFragment : Fragment() {
     ): View? {
         binding = FragmentRankingBinding.inflate(inflater, container, false)
 
+
+
+        val adapter = RankingRVAdapter()
+
+
+//        fun submitList(items: List<RankingRVAdapter.RankUser>) {
+//
+//            val adapter = RankingRVAdapter()
+//            binding.rankingEmptyTv.isVisible = false
+//            binding.rankingRecyclerView.isVisible = true
+//            adapter.set
+//
+//            if (items.isEmpty()) {
+//                    binding.rankingEmptyTv.isVisible = true
+//                    binding.rankingRecyclerView.isVisible = false
+//                }
+//        }
+
         binding.rankingRecyclerView.adapter = RankingRVAdapter()
 
         binding.rankingInfoIb.setOnClickListener {
             val intent = Intent(context, RankingInfoActivity::class.java)
             startActivity(intent)
         }
+
+
 
         return binding.root
     }
