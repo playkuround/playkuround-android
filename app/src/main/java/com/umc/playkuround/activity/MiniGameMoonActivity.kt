076d87e08e5a8925fc362ce1da11e8e9dff5b6c6
Sang-Min Kang <rdrniel12345@gmail.com>
2023-12-28 22:42:03 +0900

    feat : apply moon click motion

--- app/src/main/java/com/umc/playkuround/activity/MiniGameMoonActivity.kt
+++ app/src/main/java/com/umc/playkuround/activity/MiniGameMoonActivity.kt
@@ -2,6 +2,8 @@ package com.umc.playkuround.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.umc.playkuround.PlayKuApplication
@@ -11,6 +13,8 @@ import com.umc.playkuround.data.Ranking
 import com.umc.playkuround.databinding.ActivityMinigameMoonBinding
 import com.umc.playkuround.dialog.LoadingDialog
 import com.umc.playkuround.service.UserService
+import java.util.Timer
+import kotlin.concurrent.timer
 
 
 class MiniGameMoonActivity : AppCompatActivity() {
@@ -18,6 +22,7 @@ class MiniGameMoonActivity : AppCompatActivity() {
     private var count = 100
 
     lateinit var binding : ActivityMinigameMoonBinding
+    private var gifCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,14 +42,63 @@ class MiniGameMoonActivity : AppCompatActivity() {
 
                 //saveAdventureLog()
             }
-            else if (count == 80) {
-                binding.moonClickIv.setImageResource(R.drawable.moon_two)
+            else if (count <= 50) {
+                if(gifCount != 0) return@setOnClickListener
+                val handler = Handler(Looper.getMainLooper())
+                handler.post(object : Runnable {
+                    override fun run() {
+                        when(gifCount) {
+                            0 -> binding.moonClickIv.setImageResource(R.drawable.moon_three_1)
+                            1 -> binding.moonClickIv.setImageResource(R.drawable.moon_three_2)
+                            2 -> binding.moonClickIv.setImageResource(R.drawable.moon_three_3)
+                            3 -> binding.moonClickIv.setImageResource(R.drawable.moon_three_1)
+                        }
+                        gifCount++
+                        if(gifCount < 4)
+                            handler.postDelayed(this, 100)
+                        else
+                            gifCount = 0
+                    }
+                })
             }
-            else if (count == 50) {
-                binding.moonClickIv.setImageResource(R.drawable.moon_three)
+            else if (count <= 80) {
+                if(gifCount != 0) return@setOnClickListener
+                val handler = Handler(Looper.getMainLooper())
+                handler.post(object : Runnable {
+                    override fun run() {
+                        when(gifCount) {
+                            0 -> binding.moonClickIv.setImageResource(R.drawable.moon_two_1)
+                            1 -> binding.moonClickIv.setImageResource(R.drawable.moon_two_2)
+                            2 -> binding.moonClickIv.setImageResource(R.drawable.moon_two_3)
+                            3 -> binding.moonClickIv.setImageResource(R.drawable.moon_two_1)
+                        }
+                        gifCount++
+                        if(gifCount < 4)
+                            handler.postDelayed(this, 100)
+                        else
+                            gifCount = 0
+                    }
+                })
             }
-
-
+            else {
+                if(gifCount != 0) return@setOnClickListener
+                val handler = Handler(Looper.getMainLooper())
+                handler.post(object : Runnable {
+                    override fun run() {
+                        when(gifCount) {
+                            0 -> binding.moonClickIv.setImageResource(R.drawable.moon_1)
+                            1 -> binding.moonClickIv.setImageResource(R.drawable.moon_2)
+                            2 -> binding.moonClickIv.setImageResource(R.drawable.moon_3)
+                            3 -> binding.moonClickIv.setImageResource(R.drawable.moon_1)
+                        }
+                        gifCount++
+                        if(gifCount < 4)
+                            handler.postDelayed(this, 100)
+                        else
+                            gifCount = 0
+                    }
+                })
+            }
         }
 
         binding.moonPauseBtn.setOnClickListener {
